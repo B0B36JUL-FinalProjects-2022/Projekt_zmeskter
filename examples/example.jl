d7188aa0351e3a4f923c52c1f1676b7a6ad06af8
zmeskter <zmeskter@fel.cvut.cz>
2022-12-31 10:22:05 +0100

    ADD documentation on distance functions

--- examples/example.jl
+++ examples/example.jl
@@ -10,8 +10,8 @@ for i = 1:size(points)[1]
 end
 grid[1,:,:]
 grid[2,:,:]
-pcl_distances(delete_zero(grid[1,:,:]), delete_zero(grid[3,:,:]))
+distance(delete_zero(grid[1,:,:]), delete_zero(grid[3,:,:]))
 distance(delete_zero(grid[1,:,:]), delete_zero(grid[2,:,:]))
 distance(delete_zero(grid[2,:,:]), delete_zero(grid[1,:,:]))
-pcl_distances(delete_zero(grid[1,:,:]), Matrix{Float64}(undef,0,3))
+distance(delete_zero(grid[1,:,:]), Matrix{Float64}(undef,0,3))
 save_xyz(points, "data//test2.xyz")
--- src/distances.jl
+++ src/distances.jl
@@ -1,18 +1,34 @@
 
+"""
+    distance(points1::Array{<:Real}, points2::Array{<:Real})
+calculate the distance of two points in 3D space given as two Arrays
+# Examples
+```julia-repl
+julia> distance([1,1,1],[1,2,3])
+2.23606797749979
+```
+
+"""
 function distance(points1::Array{<:Real}, points2::Array{<:Real})
-    """
-    calculate the distance of two points in 3D space given as two Arrays
-    """
     #d = ((x2 - x1)2 + (y2 - y1)2 + (z2 - z1)2)1/2 
     x1,y1,z1 = points1
     x2,y2,z2 = points2
     return sqrt((x2 - x1)^2 + (y2 - y1)^2 + (z2 - z1)^2)
 end
 
+"""
+    distance(points1::Array{<:Real}, points2::Matrix{<:Real})
+calculate distances of points from cluster (points2) given as Matrix from one specified point (points1) given as Array
+# Examples
+```julia-repl
+julia> distance([1,1,1], [1 2 3;2 3 4;3 4 5])
+3-element Vector{Float64}:
+ 2.23606797749979
+ 3.7416573867739413
+ 5.385164807134504
+```
+"""
 function distance(points1::Array{<:Real}, points2::Matrix{<:Real})
-    """
-    calculate the distance of a custer of points (points2) given as Matrix from one specified point (points1) given as Array
-    """
     num_of_points = size(points2)[1]
     distances = zeros( num_of_points)
     for j = 1: num_of_points
@@ -20,12 +36,18 @@ function distance(points1::Array{<:Real}, points2::Matrix{<:Real})
     end
     return distances
 end
-
+"""
+    distance(points1::Matrix{<:Real},points2::Matrix{<:Real})
+calculate the distance of two clusters of points given as Matrix
+where the distance of these clusters is defined as the smallest distance between two points where each is from a different cluster
+# Examples
+```julia-repl
+julia> distance([1 1 1; 2 2 2; 3 3 3], [1 2 3; 2 3 4; 3 4 5])
+1.4142135623730951
+```
+"""
 function distance(points1::Matrix{<:Real},points2::Matrix{<:Real})
-    """
-    calculate the distance of two clusters of points given as Matrix
-    Where the distance of these clusters is defined as the smallest distance between two points when each is from a different cluster
-    """
+    
     size1 = size(points1)[1]
     size2 = size(points2)[1]
     #println(size1, "   ", size2)
@@ -39,4 +61,4 @@ function distance(points1::Matrix{<:Real},points2::Matrix{<:Real})
     return minimum(min_distances)
 end
 
-export  distance, pcl_distances
+export  distance
