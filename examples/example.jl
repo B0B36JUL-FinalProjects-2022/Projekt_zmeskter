d3bc1a4ed6ea553f362c22aff8b4076fcd068344
zmeskter <zmeskter@fel.cvut.cz>
2023-01-01 12:41:35 +0100

    add dron removal

--- examples/example.jl
+++ examples/example.jl
@@ -1,17 +1,22 @@
 using Revise # this must come before `using PCDGroundRemoval`
 using PCDGroundRemoval
 
+#load data
 points = read_xyz("data//pol.xyz")
+#compute distances from [0,0,0]
+distances = distance([0, 0, 0],points)
+#remove dron points
+points = remove_drone(points, distances)
 print(points)
+#sort points into pillar grid
 grid = make_pillar_grid(points;grid_box_size= 1.8)
+#save data after dron removal
+save_xyz(points, "data//test2.xyz")
+
+#examples of distance and removal functions calls
 delete_zero(grid[1,:,:])
-for i = 1:size(points)[1]
-    println(distance(points[i,:], [0;0;0]))
-end
-grid[1,:,:]
-grid[2,:,:]
+distance([0, 0, 0],points)
 distance(delete_zero(grid[1,:,:]), delete_zero(grid[3,:,:]))
 distance(delete_zero(grid[1,:,:]), delete_zero(grid[2,:,:]))
 distance(delete_zero(grid[2,:,:]), delete_zero(grid[1,:,:]))
 distance(delete_zero(grid[1,:,:]), Matrix{Float64}(undef,0,3))
-save_xyz(points, "data//test2.xyz")
--- src/PCDGroundRemoval.jl
+++ src/PCDGroundRemoval.jl
@@ -45,19 +45,21 @@ julia> make_pillar_grid([0 0 0; 1 1 1; 2 2 2; 1.5 1.5 1.5; 3 3 3]; grid_box_size
 """
 function make_pillar_grid(points::Matrix{<:Real}; grid_box_size = 1.8)
     x_max,y_max,x_min, y_min = my_extremas(points, grid_box_size)
-    #println(x_max, " ", x_min, " ", y_max, " ", y_min)
+    xmin = ceil(abs(minimum(points[:,1])))
+    ymin = ceil(abs(minimum(points[:,2])))
+    #println(x_max, " ", x_min, " ", y_max, " ", y_min, " ", xmin, " ", ymin)
     x_size = x_min + x_max
     y_size = y_min + y_max
     #println(x_size,"   ", y_size)
-    x = points[:,1]
-    y = points[:,2]
+    X = points[:,1]
+    Y = points[:,2]
     grid = zeros((Int64(x_size*y_size), size(points)[1], 3))
     for i = 1:x_size
         for j =1:y_size
-            x_part1 = findall(c-> c < (grid_box_size*i - x_min), x)
-            x_part2 = findall(c-> c >= (grid_box_size*(i-1) - x_min),x)
-            y_part1 = findall(c-> c < (grid_box_size*j-y_min),y)
-            y_part2 = findall(c-> c >= (grid_box_size*(j-1)-y_min),y)
+            x_part1 = findall(x-> x < (grid_box_size*i - xmin), X)
+            x_part2 = findall(x-> x >= (grid_box_size*(i-1) - xmin),X)
+            y_part1 = findall(y-> y < (grid_box_size*j-ymin),Y)
+            y_part2 = findall(y-> y >= (grid_box_size*(j-1)-ymin),Y)
             #println(x_part1, "    ", x_part2)
             x_part= intersect(x_part1,x_part2)
             y_part = intersect(y_part1,y_part2)
@@ -67,7 +69,7 @@ function make_pillar_grid(points::Matrix{<:Real}; grid_box_size = 1.8)
             grid[(i-1)*x_size + j, 1:shape[1], :] = points[common,:]
         end
     end
-    #print(grid)
+    #print(size(grid))
     return delete_zero(grid)
 
 end
@@ -80,7 +82,16 @@ function my_extremas(points::Matrix{<:Real}, grid_box_size)
     return x_max,y_max,x_min, y_min
 end
 
+function remove_drone(points::Matrix{<:Real}, distances::Array{<:Real})
+    distance_not_zero_idx = findall(dist-> dist != 0, distances)
+    distance_not_zero = distances[distance_not_zero_idx]
+    distance_smaller_than = findall(dist-> dist >= 0.35, distance_not_zero)
+    points_without_zero = points[distance_not_zero_idx,:]
+    points = points_without_zero[distance_smaller_than,:]
+    return points
+end
 
 
-export make_pillar_grid
+
+export make_pillar_grid, remove_drone
 end
