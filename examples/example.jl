a95fe6090b98cba846b42c74f012de4f8d7dc0cf
zmeskter <zmeskter@fel.cvut.cz>
2023-01-22 16:52:32 +0100

    Add clustering and bare trunk classification

--- examples/example.jl
+++ examples/example.jl
@@ -3,28 +3,36 @@ using PCDGroundRemoval
 
 #load data
 points = read_xyz("data//1651738515.007457803.xyz")
-#points = read_xyz("data//pol.xyz")
 #compute distances from [0,0,0]
 distances = distance([0, 0, 0],points)
+
 #remove dron points
 points = remove_drone(points, distances)
-print(points)
+
 #sort points into pillar grid
 grid = make_pillar_grid(points;grid_box_size= 1.8)
+
 #label ground points
 ground = label_ground_points(grid; threshold = 0.4)
 possible_ground_idx = findall(pts -> pts < 0, ground[:,3])
 ground = ground[possible_ground_idx, :]
+
 #remove ground
 without_ground = remove_ground_points(grid; threshold = 0.4)
 
 #save data after dron removal
-save_xyz(points, "data//test2.xyz")
+save_xyz(points, "data//without_ground.xyz")
+
+#clustering
+clusters_dbscan = clustering(transpose(without_ground[:,1:2]), 0.12, 2)
+clusters_ind = getproperty.(clusters_dbscan, :core_indices)
+clusters = [without_ground[i,:] for i in clusters_ind] #points sorted into clusters
 
 #find if point is in pointcloud
 pts = ground[1,:]
 all(points .== pts', dims=2)
 findall(all(points .== pts', dims=2)[:, 1])
+
 #examples of distance and removal functions calls
 delete_zero(grid[1,:,:])
 distance([0, 0, 0],points)
@@ -33,6 +41,9 @@ distance(delete_zero(grid[1,:,:]), delete_zero(grid[2,:,:]))
 distance(delete_zero(grid[2,:,:]), delete_zero(grid[1,:,:]))
 distance(delete_zero(grid[1,:,:]), Matrix{Float64}(undef,0,3))
 
+#classification + visualization
+plt = classify_bare_trunk(clusters;threshold1 = 0.1, threshold2 = 0.1, accuracy = 0.75, ignore_points = 20)
+
 #visualizations
 using Plots
 
@@ -40,3 +51,10 @@ plotlyjs()
 scatter3d(points[:,1], points[:,2], points[:, 3],color = RGB(0,1,0), markersize = 1)
 scatter3d(without_ground[:,1], without_ground[:,2], without_ground[:, 3],color = RGB(0,1,0), markersize = 1)
 scatter3d!(ground[:,1], ground[:,2], ground[:, 3], color = RGB(1,0,0),markersize = 1)
+scatter3d(cluster[51][:,1], cluster[51][:,2], cluster[51][:, 3],color = RGB(0,1,0), markersize = 1)
+scatter3d!(cluster[2][:,1], cluster[2][:,2], cluster[2][:, 3],color = RGB(1,0,0), markersize = 1)
+scatter3d!(cluster[3][:,1], cluster[3][:,2], cluster[3][:, 3],color = RGB(0,0,1), markersize = 1)
+
+
+
+
--- src/PCDGroundRemoval.jl
+++ src/PCDGroundRemoval.jl
@@ -1,9 +1,12 @@
 module PCDGroundRemoval
-
+using Clustering
+using Random
 # Write your package code here.
 include("xyzIO.jl")
 include("pointsDeleting.jl")
 include("distances.jl")
+include("circleFitting.jl")
+
 
 """
     make_pillar_grid(points::Matrix{<:Real}; grid_box_size = 1.8)
@@ -47,10 +50,8 @@ function make_pillar_grid(points::Matrix{<:Real}; grid_box_size = 1.8)
     x_max,y_max,x_min, y_min = my_extremas(points, grid_box_size)
     xmin = ceil(abs(minimum(points[:,1])))
     ymin = ceil(abs(minimum(points[:,2])))
-    #println(x_max, " ", x_min, " ", y_max, " ", y_min, " ", xmin, " ", ymin)
     x_size = x_min + x_max
     y_size = y_min + y_max
-    #println(x_size,"   ", y_size)
     X = points[:,1]
     Y = points[:,2]
     grid = zeros((Int64(x_size*y_size), size(points)[1], 3))
@@ -60,16 +61,13 @@ function make_pillar_grid(points::Matrix{<:Real}; grid_box_size = 1.8)
             x_part2 = findall(x-> x >= (grid_box_size*(i-1) - xmin),X)
             y_part1 = findall(y-> y < (grid_box_size*j-ymin),Y)
             y_part2 = findall(y-> y >= (grid_box_size*(j-1)-ymin),Y)
-            #println(x_part1, "    ", x_part2)
             x_part= intersect(x_part1,x_part2)
             y_part = intersect(y_part1,y_part2)
             common = intersect(x_part,y_part)
-            #println(common, "   ", i*x_size + j + 1, "    ",points[common,:])
             shape = size(common)
             grid[(i-1)*x_size + j, 1:shape[1], :] = points[common,:]
         end
     end
-    #print(size(grid))
     return delete_zero(grid)
 
 end
@@ -99,16 +97,13 @@ function label_ground_points(grid::Array{<:Real}; threshold = 0.45)
         sums = sum(abs.(all_points), dims = 2)
         real_points_idx = findall(pts -> pts != 0, sums[:,])
         real_points = all_points[real_points_idx,:]
-        #println(real_points, "    ", size(real_points))
         if(size(real_points)[1] == 0)
             continue
         end
 
         min_point = minimum(real_points[:, 3])
-        #println(min_point)
         ground_points_idx = findall(pts -> pts < min_point +threshold, real_points[:,3])
         ground_points = real_points[ground_points_idx,:]
-        #println(ground_points)
         ground_pts =vcat(ground_pts, ground_points)
         ground_pts =unique(ground_pts, dims = 1)
 
@@ -125,18 +120,15 @@ function remove_ground_points(grid::Array{<:Real}; threshold = 0.45)
         sums = sum(abs.(all_points), dims = 2)
         real_points_idx = findall(pts -> pts != 0, sums[:,])
         real_points = all_points[real_points_idx,:]
-        #println(real_points, "    ", size(real_points))
         if(size(real_points)[1] == 0)
             continue
         end
 
         min_point = minimum(real_points[:, 3])
-        #println(min_point)
         ground_points_idx = findall(pts -> pts >= min_point +threshold, real_points[:,3])
         ground_points = real_points[ground_points_idx,:]
         possible_ground_idx = findall(pts -> pts >= 0, ground_points[:,3])
         ground_points = ground_points[possible_ground_idx, :]
-        #println(ground_points)
         ground_pts =vcat(ground_pts, ground_points)
         ground_pts =unique(ground_pts, dims = 1)
 
@@ -145,8 +137,10 @@ function remove_ground_points(grid::Array{<:Real}; threshold = 0.45)
 
 end
 
+function clustering(points, eps, min)
+    return dbscan(points, eps, min_neighbors = min, min_cluster_size = 20)
+end
 
 
-
-export make_pillar_grid, remove_drone, label_ground_points, remove_ground_points
+export make_pillar_grid, remove_drone, label_ground_points, remove_ground_points, clustering, RANSAC, center, fit_circle, fit_dist, my_extremas
 end
