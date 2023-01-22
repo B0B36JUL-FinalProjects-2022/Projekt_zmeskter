2416e7c38186d0c17e2688bcb92651d2d4735eda
zmeskter <zmeskter@fel.cvut.cz>
2023-01-22 16:32:33 +0100

    Add circleFitting doc

--- src/circleFitting.jl
+++ src/circleFitting.jl
@@ -1,4 +1,9 @@
 using Random
+"""
+    function fit_circle(X)
+
+take Nx2 matrix of points and returns the coefficients d,e,f of the circle given by the equation x^2 + y^2 + dx + ey + f = 0
+"""
 function fit_circle(X)
     N, M = size(X)
     A = hcat(X[:,1], X[:,2], ones(N,1))
@@ -9,7 +14,12 @@ function fit_circle(X)
     f = K[3]
     return d,e,f
 end
+"""
+    function center(d,e,f)
 
+Calculate the coordinates of the centre (x0,y0) and radius (r) of the circle from 
+the parameters d,e,f that correspond to the equation of the circle x^2 + y^2 + dx + ey + f = 0.
+"""
 function center(d,e,f)
     x0 = -d/2
     y0 = -e/2
@@ -17,8 +27,14 @@ function center(d,e,f)
     return x0,y0,r
 end
 
+"""
+    function fit_dist(X,x0,y0,r)
+
+Calculate the distance of the point specified by the Nx2 matrix from the circle centered at [x0,y0] with radius r.
+The points inside the circle will have negative distance, those outside will have positive distance.
+"""
 function fit_dist(X,x0,y0,r)
-    N,M = size(X)
+    N = size(X)[1]
     d = zeros(N)
     for i = 1:N
         d[i] = sqrt((X[i,1] - x0)^2 + (X[i,2] - y0)^2) - r
@@ -26,6 +42,15 @@ function fit_dist(X,x0,y0,r)
     return d
 end
 
+"""
+    function RANSAC(X, num_iter, threshold)
+
+Implementation of the RANSAC method for interpolating points with a circle.
+    It takes the points given by Nx2Matrix, selects 3 random points and intersects the circle with them. 
+    Calculate the distances of all points from this circle. 
+    If the distance of a point from the circle is less than a threshold, declare it an inlier. 
+    Repeat this process num_iter times. Finally, it selects the circle that had the most inliners.
+"""
 function RANSAC(X, num_iter, threshold)
     d = 0
     e = 0
