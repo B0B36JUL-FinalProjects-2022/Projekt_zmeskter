61cf780f3f2b8385d3ff880c0d59bfccba750c96
zmeskter <zmeskter@fel.cvut.cz>
2023-01-23 17:20:39 +0100

    small fixes

--- src/classify.jl
+++ src/classify.jl
@@ -6,11 +6,16 @@ using Plots
 take NxMx3 array of points sorted into clusters and return plot image showing which clusters contain the tree (bare trunk)
 these are shown in green in the picture. Clusters containing less than xxx points are coloured blue 
 and the remaining clusters are coloured red - they do not contain a tree, but are large enough to be a major object.
-#Optional params:
-    threshold1: for RANSAC -specifies the distance from the circle within which the point is taken as its inliner
-    threshold2: tolerance of the distance from the circle to take the point as part of the trunk
-    accuracy: what is the required percentage of points within the tolerance (treshold2) for the cluster to be considered a tree
-    ignore_points: minimum amount of points in the cluter to be considered
+
+# Optional params:
+
+threshold1: for RANSAC -specifies the distance from the circle within which the point is taken as its inliner
+
+threshold2: tolerance of the distance from the circle to take the point as part of the trunk
+
+accuracy: what is the required percentage of points within the tolerance (treshold2) for the cluster to be considered a tree
+
+ignore_points: minimum amount of points in the cluter to be considered
 
 """
 function classify_bare_trunk(clusters; threshold1 = 0.1, threshold2 = 0.1, accuracy= 0.75, ignore_points = 20)
@@ -29,14 +34,18 @@ end
     get_color(cluster,r, threshold, accuracy, ignore_points)
 
 take Nx3 matrix of points from one cluster and returns the color that corresponds to how it was classified:
-    green: tree (bare trunk)
-    red: not tree, but objet with enough points
-    blue: too small to be considered
-#Params:
-    cluster: Nx3 matrix of points in cluster
-    r: radius of fitting circle (tree radius)
-    threshold: tolerance of the distance from the circle to take the point as part of the trunk
-    ignore_points: minimum amount of points in the cluter to be considered
+
+green: tree (bare trunk), red: not tree, but objet with enough points, blue: too small to be considered
+
+# Params:
+
+cluster: Nx3 matrix of points in cluster
+
+r: radius of fitting circle (tree radius)
+
+threshold: tolerance of the distance from the circle to take the point as part of the trunk
+
+ignore_points: minimum amount of points in the cluter to be considered
 """
 function get_color(cluster,x0,y0,r, threshold, accuracy, ignore_points)
     dist = distance([x0,y0], cluster[:,1:2])
